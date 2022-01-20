9034672a76219e5b83972c196fddfeb52e0dbe6a
bcassamo <banucho.cassamo@gmail.com>
2022-01-20 21:48:30 +0200

    Criacao da tela de pesquisa de vendas

---
+++ src/app/pesquisa-venda/pesquisa-venda.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pesquisa-venda',
+  templateUrl: './pesquisa-venda.component.html',
+  styleUrls: ['./pesquisa-venda.component.css']
+})
+export class PesquisaVendaComponent {
+
+  business = [
+
+  ];
+
+  valorTotal = 50;
+
+}
