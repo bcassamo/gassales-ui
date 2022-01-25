d650b6091202e7b209f1ca48cf68d13cbb6f3722
bcassamo <banucho.cassamo@gmail.com>
2022-01-25 15:45:58 +0200

    melhoramento de vendas

--- src/app/nova-venda/nova-venda.component.ts
+++ src/app/nova-venda/nova-venda.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class NovaVendaComponent {
 
   lancamentos = [
-    { descricao: 'Venda', dataLancamento: '2022-01-18', estado: 'CHEIA', preco: 850.00, quantidade: 2, valorTotal: 1700, produto: 1 },
-    { descricao: 'Aquisição', dataLancamento: '2022-01-20', estado: 'CHEIA', preco: 1200.00, quantidade: 20, valorTotal: 2400, produto: 3 },
-    { descricao: 'Venda', dataLancamento: '2022-01-18', estado: 'CHEIA', preco: 1200.00, quantidade: 2, valorTotal: 2400, produto: 3 }
+    { descricao: 'Venda', dataLancamento: new Date(2022, 1, 18), estado: 'CHEIA', preco: 850.00, quantidade: 2, valorTotal: 1700, produto: 1 },
+    { descricao: 'Aquisição', dataLancamento: new Date(2022, 1, 20), estado: 'CHEIA', preco: 1200.00, quantidade: 20, valorTotal: 2400, produto: 3 },
+    { descricao: 'Venda', dataLancamento: new Date(2022, 1, 18), estado: 'CHEIA', preco: 1200.00, quantidade: 2, valorTotal: 2400, produto: 3 }
   ];
 
   valorT = 15000;
