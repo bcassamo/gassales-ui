6f8475639b2a292aa708ae67266f5b7e0b1854e2
bcassamo <banucho.cassamo@gmail.com>
2022-03-20 10:41:38 +0200

    bug fixes

--- src/app/produtos/produtos.module.ts
+++ src/app/produtos/produtos.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -14,6 +15,7 @@ import { PesquisaProdutosComponent } from './pesquisa-produtos/pesquisa-produtos
   ],
   imports: [
     CommonModule,
+    FormsModule,
 
     TableModule,
     ButtonModule,
