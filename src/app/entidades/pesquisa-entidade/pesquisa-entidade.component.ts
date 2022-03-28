1e51e4fc9ca07397f9613208ecd941b9792b4da6
bcassamo <banucho.cassamo@gmail.com>
2022-03-28 21:57:29 +0200

    criacao do componente de pesquisa de entidades

---
+++ src/app/entidades/pesquisa-entidade/pesquisa-entidade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pesquisa-entidade',
+  templateUrl: './pesquisa-entidade.component.html',
+  styleUrls: ['./pesquisa-entidade.component.css']
+})
+export class PesquisaEntidadeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
