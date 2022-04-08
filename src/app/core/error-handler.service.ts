d43667b39e14f3f47568a88ea802b857751cbe2a
bcassamo <banucho.cassamo@gmail.com>
2022-04-08 22:46:54 +0200

    Melhorando o error Handler

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
 
 import { MessageService } from 'primeng/api';
 
@@ -14,6 +15,15 @@ export class ErrorHandlerService {
 
     if(typeof errorResponse === 'string') {
       msg = errorResponse;
+    } else if(errorResponse instanceof HttpErrorResponse
+      && errorResponse.status >= 400 && errorResponse.status <= 499) {
+        msg = 'Ocorreu um erro ao processar a sua solicitação';
+
+        try {
+          msg = errorResponse.error[0].mensagemUsuario;
+        } catch (e) { }
+
+        console.error('Ocorreu um erro', errorResponse);
     } else {
       msg = 'Erro ao processar o serviço. Tente novamente.';
       console.log('Ocorreu um erro', errorResponse);
